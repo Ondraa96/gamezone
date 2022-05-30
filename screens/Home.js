e178a9598b60ed5815f6320d1aca7cdf71113155
Ondřej Vít <ondra.vit@getfix.cz>
2022-05-30 17:08:22 +0200

    navigation via props

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home screen</Text>
+            <Button title='Go to review details' onPress={() => navigation.navigate('Review details')} />
         </View>
     )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails screen</Text>
+            <Button title='Go back' onPress={() => navigation.goBack()} />
         </View>
     )
 }
