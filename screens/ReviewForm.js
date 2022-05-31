4fa5813fda0ae8cb77ba3dc68f2a54ddd0bce32f
Ondřej Vít <ondra.vit@getfix.cz>
2022-05-31 16:24:22 +0200

    realtime validation

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, Button, View } from 'react-native';
+import { TextInput, Button, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { Formik } from 'formik';
 import * as yup from 'yup';
@@ -36,20 +36,26 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review title'
                             onChangeText={props.handleChange('title')}
                             value={props.values.title}
+                            onBlur={props.handleBlur('title')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
                         <TextInput
                             style={globalStyles.input}
                             placeholder='Review body'
                             onChangeText={props.handleChange('body')}
                             value={props.values.body}
+                            onBlur={props.handleBlur('body')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
                         <TextInput
                             style={globalStyles.input}
                             placeholder='Rating (1-5)'
                             onChangeText={props.handleChange('rating')}
                             value={props.values.rating}
                             keyboardType='numeric'
+                            onBlur={props.handleBlur('rating')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
                         <Button title='Submit' color='maroon' onPress={props.handleSubmit} />
                     </View>
                 )}
